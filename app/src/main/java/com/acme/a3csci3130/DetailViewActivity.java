a43a3902d66eb07062ddd7348facb8772d5c9104
L Toal <lc790935@dal.ca>
2018-06-30 20:44:42 -0300

    Added the ability to update an entry.

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -9,7 +9,7 @@ import android.widget.Spinner;
 
 public class DetailViewActivity extends Activity {
 
-    private EditText nameField, addressField;
+    private EditText nameField, numberField, addressField;
     private Spinner typeField, provinceField;
     Contact receivedPersonInfo;
 
@@ -20,16 +20,37 @@ public class DetailViewActivity extends Activity {
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
 
         nameField = (EditText) findViewById(R.id.businessname);
+        numberField = (EditText) findViewById(R.id.businessnumber);
+        typeField = (Spinner) findViewById(R.id.businesstype);
         addressField = (EditText) findViewById(R.id.businessaddress);
+        provinceField = (Spinner) findViewById(R.id.businessprovince);
 
         if(receivedPersonInfo != null){
             nameField.setText(receivedPersonInfo.businessname);
+            for (int i=0;i<typeField.getCount();i++){
+                if (typeField.getItemAtPosition(i).toString().equals(receivedPersonInfo.businesstype)){
+                    typeField.setSelection(i);
+                }
+            }
+            numberField.setText(receivedPersonInfo.businessnumber);
             addressField.setText(receivedPersonInfo.businessaddress);
+            for (int i=0;i<provinceField.getCount();i++){
+                if (provinceField.getItemAtPosition(i).toString().equals(receivedPersonInfo.businessprovince)){
+                    provinceField.setSelection(i);
+                }
+            }
         }
     }
 
     public void updateContact(View v){
         //TODO: Update contact funcionality
+        receivedPersonInfo.businessname = nameField.getText().toString();
+        receivedPersonInfo.businessnumber = numberField.getText().toString();
+        receivedPersonInfo.businesstype = typeField.getSelectedItem().toString();
+        receivedPersonInfo.businessaddress = addressField.getText().toString();
+        receivedPersonInfo.businessprovince = provinceField.getSelectedItem().toString();
+        MyApplicationData appData = (MyApplicationData)getApplication();
+        appData.firebaseReference.child(receivedPersonInfo.businessid).setValue(receivedPersonInfo);
     }
 
     public void eraseContact(View v)
